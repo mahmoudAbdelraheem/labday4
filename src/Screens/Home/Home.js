6e8d1a134907b103e959925d2305d427294e7c11
mahmoudAbdelraheem <mmoud2031@gmail.com>
2024-09-14 22:37:10 +0200

    home banar card

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -1,18 +1,29 @@
-import { View, Text, FlatList, ScrollView, Pressable } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  ScrollView,
+  Pressable,
+  ImageBackground,
+} from "react-native";
 import styles from "./styles";
 import { SafeAreaView } from "react-native-safe-area-context";
 import HomeHeader from "../../Components/HomeHeader/HomeHeader";
 import MovieCard from "../../Components/MovieCard/MovieCard";
+import Entypo from "@expo/vector-icons/Entypo";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { useEffect, useState } from "react";
 import {
   getPopularMovies,
   getTopRatedMovies,
   getNowPlayingMovies,
 } from "./config";
+import { ApiEndPoints } from "../../core/EndPoints";
 export default function Home({ navigation }) {
   const [popularMovies, setPopularMovies] = useState([]);
   const [topRatedMovies, setTopRatedMovies] = useState([]);
   const [playingMovies, setPlayingMovies] = useState([]);
+  const [banarImageIndex, setBanarImageIndex] = useState(0);
   getHomeMovies = async () => {
     var data;
     data = await getPopularMovies();
@@ -22,14 +33,43 @@ export default function Home({ navigation }) {
     data = await getNowPlayingMovies();
     setPlayingMovies(data);
   };
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
   useEffect(() => {
     getHomeMovies();
+
+    let randomInt = getRandomInt(1, 20);
+    setBanarImageIndex(randomInt);
   }, []);
 
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
         <HomeHeader />
+        <View style={styles.banar}>
+          <ImageBackground
+            style={styles.banarImage}
+            source={{
+              uri:
+                ApiEndPoints.imageBaseUrl +
+                popularMovies[banarImageIndex]?.backdrop_path,
+            }}
+          >
+            <View style={styles.banarBtn}>
+              <View style={styles.banarPlayBtn}>
+                <Entypo name="controller-play" size={28} color="black" />
+                <Text style={styles.banarPlayBtnText}>Play</Text>
+              </View>
+              <View style={styles.banarListBtn}>
+                <MaterialIcons name="done" size={28} color="white" />
+                <Text style={styles.banarMyListBtnText}>My List</Text>
+              </View>
+            </View>
+            <Text style={styles.banarText}>For Mahmoud.</Text>
+          </ImageBackground>
+        </View>
         <Text style={styles.textHeader}>Playing Now</Text>
         <FlatList
           data={playingMovies}
